3dda1917859d515415e3057a502daaddeabe744d
aghassaei <aghassaei@reed.edu>
2024-03-21 19:13:22 -0700

    Commit

--- mancala.py
+++ mancala.py
@@ -4,12 +4,22 @@ import numpy as np
 class Player(i):
     def __init__(self, i):
         self.legal = range(6) if i == 1 else range(6,12)
+        self.store = 0
 
-class Game(player1, player2):
+
+class Board():
+    def __init__(self, side1 = np.ones(6) * 4, side2 = np.ones(6) * 4):
+        self.side1 = side1
+        self.side2 = side2
+        self.store1 = 0
+        self.store2 = 0
+        self.player = True
+
+class Game():
     def __init__(self, player1, player2):
-        self.board = np.ones(14) * 4
-        self.player1 = player1
-        self.player2 = player2
+        self.board = Board()
+        self.player1 = Player(1)
+        self.player2 = Player(2)
         self.log
     
     def move(self, i, player):
@@ -27,4 +37,8 @@ class Game(player1, player2):
         # adj side cleared
         # pieces added to your 
         pass
-
+    
+    # def is_legal(self, i):
+    #     if (self.board.player and i in range(6) or
+    #         not self.board.player and i in range():
+    #         True
