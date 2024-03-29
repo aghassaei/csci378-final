3d7068f12c1416be4fbeafc9fb1b741cf690f1e8
aghassaei <aghassaei@reed.edu>
2024-03-29 09:19:38 -0700

    Commit

--- README.md
+++ README.md
@@ -1,4 +1,32 @@
 # CSCI 378 Final Project
 
+## Goal
+My goal is to implement Mancala. Mancala is a two-player strategy game. The board consists of 12 cups and two stores on either side. Below is an ASCI
+representation of the board with each cup and store assigned an index:
+
+
+
+    -----------------------------------------------------------
+    |    |   12  |   11  |   10  |   09  |   08  |   07  |    |
+    | 13 |-----------------------------------------------| 06 | 
+    |    |   00  |   01  |   02  |   03  |   04  |   05  |    |
+    -----------------------------------------------------------
+
+
+Player 1 is assigned store 06 and player 2 is assigned store 13. Four stones are distributed in each cup to start.
 ## Rules of the game
 1. 6 holes on each side. Each player has a mancala
+
+
+<!-- 
+    the problem you're trying to solve or the area you're studying,
+    a brief summary of what you learned from your background reading,
+    what you implemented and why you chose to implement that, and
+    how well your implementation worked (and how you measured it).
+ -->
+
+
+## References
+https://huggingface.co/learn/deep-rl-course/en/unit7/self-play
+
+https://en.wikipedia.org/wiki/Mancala
--- mancala.py
+++ mancala.py
@@ -2,21 +2,93 @@ import numpy as np
 
 ### MANCALA ####
 
-
-# Returns cup index on opposite side of the board
-def adj_cup(n):
-    return 12 - n
+# is updating the state the way i am bad?
 
 
-class Game:
+class GameEnv:
     def __init__(self):
-        self.board = [4,4,4,4,4,4,0,4,4,4,4,4,4,0]
-        self.log = []
-        self.player1 = True
         self.state = None
         self.count = 0
-        self.rng = np.random.default_rng()
+        self.rng = np.random.randint()
         self.limit = 200 
+        # self.state_dim = ????
+        self.num_actions = 6
+
+    def reset(self):
+        self.state = np.arrray([4,4,4,4,4,4,0,4,4,4,4,4,4,0])
+        self.count = 0
+        return self.state
+    
+    def distribute(self, i, pieces, exclude=13):
+        while pieces > 0:
+            if i == exclude:
+                continue
+            self.state[i] += 1
+            i += 1
+            pieces -= 1
+        return i
+    
+    def capture(self, i, store):
+        adj = 12 - i
+        self.state[store] += self.state[adj]
+        self.state[adj] = 0
+    
+    def rand_agent(self, store=13):
+        while True:
+            action = self.rng()
+            pieces = self.state[action]
+
+            self.state[action] = 0
+
+            self.state, last_idx = self.distribute(self.state, action + 1, pieces, exclude=store)
+
+            if self.state[last_idx] == 1 and last_idx != 13:
+                self.state = self.capture(self.state, last_idx, 13)
+            if last_idx != 13:
+                return
+    
+    
+    def step(self, action):
+        self.count += 1
+
+        # Learner's turn. Start by taking pieces out of selected cup
+        pieces = self.state[action]
+        self.state[action] = 0
+
+        # Distribute pieces
+        last_idx = self.distribute(action + 1, pieces, 6)
+
+        # Instead of going again, we skip opponent's turn
+        if last_idx == 6:
+            return self.end_state()
+
+        # Capture
+        if self.state[last_idx] == 1:
+            self.state = self.capture(last_idx, 6)
+
+        # Opponent's turn
+        
+        self.rand_agent()
+
+
+        return self.end_state()
+    
+    def end_state(self):
+        terminated = self.state[6] > 24 or self.state[13] > 24
+        truncated = self.count > self.limit
+        if self.state[6] > self.state[13]:
+            reward = 1
+        else:
+            reward = 0
+        return self.state, reward, terminated, truncated, {}
+    
+
+
+
+
+
+
+
 
     @staticmethod
     def format(x):
@@ -27,65 +99,49 @@ class Game:
     def view(self):
         output = " -----------------------------------\n|  |"
         for i in range(12,6, -1): 
-            output.join([" ", format(self.board[i]), " |"])
+            output.join([" ", format(self.self.state[i]), " |"])
         
         output.join(["|  |\n"])
 
         for i in range(6): 
-            output.join([" ", format(self.board[i]), " |"])
+            output.join([" ", format(self.self.state[i]), " |"])
 
-        print("Scoreboard") # TODO
+        print("Scoreself.state") # TODO
         print(output)
         return None
     
-    def move(self, i):
-        if self.in_bounds(i) and self.not_empty(i):
-            pieces = self.board[i]
-            self.board[i] = 0
-            idx = i
-            while pieces > 0:
-                if (self.player1 and idx == 13) or (not self.player1 and idx == 6):
-                    continue
-                self.board[idx] += 1
-                idx += 1
-            if (self.player1 and idx == 13) or (not self.player1 and idx == 6):
-                # go again
-                pass
+    # def move(self, i):
+    #     if self.in_bounds(i) and self.not_empty(i):
+    #         pieces = self.self.state[i]
+    #         self.self.state[i] = 0
+    #         idx = i
+    #         while pieces > 0:
+    #             if (self.player1 and idx == 13) or (not self.player1 and idx == 6):
+    #                 continue
+    #             self.self.state[idx] += 1
+    #             idx += 1
+    #         if (self.player1 and idx == 13) or (not self.player1 and idx == 6):
+    #             # go again
+    #             pass
 
-            if self.board[idx] == 1 and self.in_bounds(i): 
-                self.capture(adj_cup(idx))
-        self.player = not self.player
-    def capture(i):
-        pieces = self.board[i]
-        self.board[i] = 0
-        if self.player1:
-            self.board[6] += pieces
-        else:
-            self.board[13] += pieces
-        return 
+    #         if self.self.state[idx] == 1 and self.in_bounds(i): 
+    #             self.capture(adj_cup(idx))
+    #     self.player = not self.player
+
     
-    def in_bounds(self, i):
-        rightside_1 = self.player1 and i in range(6)
-        rightside_2 = not self.player1 and i in range(7,13)
-        stores = (i == 6) or (i == 13)
-        return rightside_1 and rightside_2 and not stores
+    # def in_bounds(self, i):
+    #     rightside_1 = self.player1 and i in range(6)
+    #     rightside_2 = not self.player1 and i in range(7,13)
+    #     stores = (i == 6) or (i == 13)
+    #     return rightside_1 and rightside_2 and not stores
     
-    def not_empty(self, i):
-        return (self.player == 0 and self.board.side0[i] > 0) or (self.player == 1 and self.board.side1[i] > 0)
+    # def not_empty(self, i):
+    #     return (self.player == 0 and self.self.state.side0[i] > 0) or (self.player == 1 and self.self.state.side1[i] > 0)
     
-    def end_game(self):
-        pass
+    # def end_game(self):
+    #     pass
     
-    def play(self):
-        g = Game()
+    # def play(self):
+    #     g = Game()
 
 
-    def reset(self, random=True):
-        if random:
-            # reset randomly
-            pass
-
-
-
-    def step(self):
-        pass
